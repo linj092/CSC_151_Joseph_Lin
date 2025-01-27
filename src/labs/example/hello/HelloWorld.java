dab557dd55ebf7f19ae4a3d597c2fc33556e572d
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2025-01-27 11:58:55 -0500

    mammal

---
+++ src/labs/example/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+ /*
+ @author: Joseph Lin
+ @date: 1/27/2025
+ @purpose: My first java program
+
+ Note:...
+
+ */
+package labs.example.hello;
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/labs/example/mammals/Mammal.java
@@ -0,0 +1,57 @@
+
+package labs.example.mammals;
+
+public class Mammal{
+
+    protected String hairColor;
+    protected String eyeColor;
+    protected String bodyTemp;
+    protected String tailLength;
+    protected String bodySize;
+    protected String furLength;
+    protected String name;
+
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void run(){
+    System.out.println("The " + this.name + " is running...");
+    }
+
+    public void eat(){
+    System.out.println("The " + this.name + " is eating...");
+    }
+
+    public void sleep(){
+    System.out.println("The " + this.name + " is sleeping...");
+    }
+
+    public void scratch(){
+    System.out.println("The " + this.name + " is scratching...");
+    }
+
+    public void bite(){
+    System.out.println("The " + this.name + " is biting...");
+    }
+
+    public void roll(){
+    System.out.println("The " + this.name + " is rolling...");
+    }
+    
+    public void cry(){
+    System.out.println("The " + this.name + " is crying...");
+    }
+    
+    public void sit(){
+    System.out.println("The " + this.name + " is sitting...");
+    }
+
+    public void jump(){
+    System.out.println("The " + this.name + " is jumping...");
+    }
+}
+
+//bite, cry, roll, sit, jump 
+//javac src/labs/example/mammals/Mammal.java 
