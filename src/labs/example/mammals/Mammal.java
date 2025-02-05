12fdb131762169264d7b200af1b74c636ec8de6c
Joseph Lin <linj092@mail.sandhills.edu>
2025-02-05 11:40:39 -0500

    a

--- src/labs/example/mammals/Mammal.java
+++ src/labs/example/mammals/Mammal.java
@@ -92,17 +92,33 @@ public class Mammal{
     System.out.println("The " + this.name + " is standing and barking");
     }
 
-    public void walk(){
+    // Optional 
+    public double walk(){
+    //creates the public method for walk
         int South = 3;
-        int East = 4; 
+        int East = 4;
+    //creates two integars to store the values 3 and 4
 
     System.out.println("The " + this.name + " walks " + South + " miles South and " + East + " miles East.");
+    //prints out the statement of how far the mammal walks using the given values
 
     double Hypotenuse = Math.sqrt((South * South) + (East * East));
-    double theta = Math.toDegrees(Math.acos(East / Hypotenuse));
-    double thetaFinal = (double)Math.round(theta * 100) / 100;
-    System.out.println("Theta of the longest leg is " + thetaFinal);
+    //creates a double (to store decimals) and uses the a^2 + b^2 = c^2 to find the hypotenuse using the math sqrt that is built in to vs.
     
+    System.out.println("The mammal is " + Hypotenuse + " miles from the starting point");
+    //states that the mammal is X from the starting point
+
+    double theta = Math.toDegrees(Math.acos(East / Hypotenuse));
+    //creates a double variable to contain decimals. Uses the built-in cosine function and uses the built in degrees converter to change the answer into degrees
+
+    double thetaFinal = (double)Math.round(theta * 100) / 100;
+    //creates the final double variable to round the theta into the nearest two decimal places. Because the math round built-in function results in an integar
+    //another double is placed to convert it into decimal form. Multiply by 100 and divide by 100 for the rounding to work because by dividing by 100 the decimal
+    //moves right to fit the 0s
+
+    System.out.println("Theta of the longest leg is " + thetaFinal + " miles");
+    //prints out the final result for theta 
+        return thetaFinal;
     }
 
     
