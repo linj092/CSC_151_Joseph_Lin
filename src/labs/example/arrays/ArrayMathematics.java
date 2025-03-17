1c077929078e9f14e217520ca8c150521b725004
Joseph Lin <linj092@mail.sandhills.edu>
2025-03-17 11:52:00 -0400

    aa

---
+++ src/labs/example/arrays/ArrayMathematics.java
@@ -0,0 +1,117 @@
+package labs.example.arrays;
+
+public class ArrayMathematics {
+    public void arrayComparison(int[] firstArray, int[] secondArray){
+        int length = 0; 
+        int checker = 0;
+        if (firstArray.length == secondArray.length){
+//compares both to determine if the lengths are similar
+            System.out.println("These arrays are the same length");
+         } else {
+            System.out.println("These arrays are not the same length");
+        }
+        if (firstArray.length > secondArray.length){
+            length = secondArray.length;
+        } else if (secondArray.length > firstArray.length){
+            length = firstArray.length;
+        } else {
+            length = firstArray.length;
+        }
+//used to determine which one has to lowest length so when values are compared it will not be out of bounds
+        for (int i = 0; i < length; i++){
+            if (firstArray[i] != secondArray[i]){
+                checker++;
+            }
+        }
+        if (checker++ > 0){
+            System.out.println("These arrays do not contain the same values");
+        } else {
+            System.out.print("These arrays contain the same values");
+        }
+        System.out.println("");
+//uses checker to record if they have the same value and to know what to print out after the loop is ran
+    }
+
+
+    public void arraySum(int[] arr1, int[] arr2, int[] arr3){
+        int arr1Sum = 0;
+        int arr2Sum = 0;
+        int arr3Sum = 0;
+        for (int i = 0; i < arr1.length; i++){
+            arr1Sum = arr1Sum + arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++){
+            arr2Sum = arr2Sum + arr2[i];
+        }
+        for (int i = 0; i < arr3.length; i++){
+            arr3Sum = arr3Sum + arr3[i];
+        }
+        //finds sum of all the arrays
+        System.out.println("The 1st array's sum is " + arr1Sum);
+        System.out.println("The 2nd array's sum is " + arr2Sum);
+        System.out.println("The 3rd array's sum is " + arr3Sum);
+        //prints the sums
+        if (arr1Sum > arr2Sum && arr1Sum > arr3Sum){
+            System.out.print("The 1st array has the largest sum");
+        } else if (arr2Sum > arr1Sum && arr2Sum > arr3Sum){
+            System.out.print("The 2nd array has the largest sum");
+        } else if (arr3Sum > arr1Sum && arr3Sum > arr2Sum){
+            System.out.print("The 3rd array has the largest sum");
+        } else {
+            System.out.println("They are equal");
+        }
+        //compares to find the largest else equal
+        System.out.println("");
+    }
+
+    public void largestSumInArray(int[] myArray){
+        int j = 0;
+        for (int i = 0; i < myArray.length; i++){
+            if (j < myArray[i]){
+                j = myArray[i];
+            }
+        }
+        System.out.println("The largest value in myArray is: " + j);
+    }
+
+    public void arrayMod10Check(int[] cardNumber){
+        int temp = 0;
+        int doubleDigitCheck = 0;
+        int doubleDigitCheck2 = 0;
+        int doubleDigitSum = 0;
+        int cardSum = 0;
+        int cardValidCheck = 0;
+        for (int i = 0; i < cardNumber.length; i++){
+        if ((i + 1) % 2 == 0){
+            cardSum = cardNumber[i] + cardSum;
+//if at an even position, add the cardnumber directly to the cardsum
+        }
+            else if ((i + 1) % 2 == 1){
+//if at an odd number
+                temp = cardNumber[i] * 2;
+//temp = cardnumber times 2
+                if (temp > 9){
+//if the resulting temp is greater than 9
+                doubleDigitCheck = temp % 10;
+//find the first digit using the remainder after dividing by 10
+                doubleDigitCheck2 = temp / 10;
+//find the second digit by dividing by 10
+                doubleDigitSum = doubleDigitCheck + doubleDigitCheck2; 
+//adds it together
+                cardSum = doubleDigitSum + cardSum;
+//add that to cardsum
+                } else {
+                cardSum = temp + cardSum;
+//if temp is not over 9 add it directly to cardsum
+                }
+        }
+    }
+    cardValidCheck = cardSum % 10;
+        if (cardValidCheck == 0){
+            System.out.println("This is a valid card");
+        } else {
+            System.out.println("This is not a valid card");
+        }
+        System.out.println(cardSum);
+}
+}
--- src/labs/example/arrays/Main.java
+++ src/labs/example/arrays/Main.java
@@ -2,10 +2,24 @@ package labs.example.arrays;
 
 public class Main {
     public static void main(String[] args) {
-    int input[] = new int[]{1, 3, 5, 8, 9, 23, 13, 50, 49, 101, 300, 501, 593, 7, 310, 394, 591, 78, 39, 10, 43, 601, 31, 95, 91, 99, 59, 39, 99, 130, 121, 302, 400, 543, 333, 664, 777, 333, 999, 110, 304, 405, 605, 303, 201, 500, 607, 103, 609, 800};
-    ArrayOperations Array = new ArrayOperations();
-    Array.createNewArray(6);
-    Array.sortArray(input);
-    Array.getDaysAndMonths();
+    //int input[] = new int[]{1, 3, 5, 8, 9, 23, 13, 50, 49, 101, 300, 501, 593, 7, 310, 394, 591, 78, 39, 10, 43, 601, 31, 95, 91, 99, 59, 39, 99, 130, 121, 302, 400, 543, 333, 664, 777, 333, 999, 110, 304, 405, 605, 303, 201, 500, 607, 103, 609, 800};
+    //ArrayOperations Array = new ArrayOperations();
+    //Array.createNewArray(6);
+    //Array.sortArray(input);
+    //Array.getDaysAndMonths();
+    int firstArrayInput[] = new int[]{4, 6, 8, 1, 2, 4, 34, 23, 61, 71, 24, 12, 41, 11, 51, 1, 2, 3, 4, 71, 12, 11, 21, 41, 61};
+    int secondArrayInput[] = new int[]{4, 6, 8, 1, 2, 4, 34, 23, 61, 71, 24, 12, 41, 11, 51, 1, 2, 3, 4, 71, 12, 11, 21, 41, 61, 51, 31};
+    int thirdArrayInput[] = new int[]{4, 6, 8, 1, 2, 4, 34, 23, 61, 71, 24, 12, 41, 11, 51, 1, 2, 3, 4, 71, 12, 11, 21, 41, 61, 34, 141, 52, 13};
+    int myArrayInput[] = new int[]{4, 6, 8, 1, 2, 4, 34, 23, 61, 71, 24, 12, 41, 11, 51, 1, 2, 3, 4, 71, 12, 11, 21, 41, 61, 34, 141, 52, 13, 5, 1, 3, 5, 6, 15, 51, 12, 616, 21, 34, 52, 31, 61, 73, 99, 12, 34, 61, 1, 61, 72, 84, 95, 12, 43, 45};
+    ArrayMathematics ArrayMath = new ArrayMathematics();
+    ArrayMath.arrayComparison(firstArrayInput, secondArrayInput);
+    ArrayMath.arraySum(firstArrayInput, secondArrayInput, thirdArrayInput);
+    ArrayMath.largestSumInArray(myArrayInput);
+    int creditCardTest1[] = new int[]{4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    int creditCardTest2[] = new int[]{5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 4, 4,};
+    int creditCardTest3[] = new int[]{3, 7, 8, 2, 8, 2, 2, 4, 6, 3, 1, 0, 0, 0, 5};
+    ArrayMath.arrayMod10Check(creditCardTest1);
+    ArrayMath.arrayMod10Check(creditCardTest2);
+    ArrayMath.arrayMod10Check(creditCardTest3);
     }
 }
